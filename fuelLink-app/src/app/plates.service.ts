0b171e98ecbbc794f1fc56e2488bfe489640bb4e
CatarinaBarroqueiro <95221731+CatarinaBarroqueiro@users.noreply.github.com>
2024-03-17 17:58:31 +0000

    Vans Mockups

--- fuelLink-app/src/app/plates.service.ts
+++ fuelLink-app/src/app/plates.service.ts
@@ -5,17 +5,55 @@ import { VehicleInfo } from './vehicle-info';
 })
 export class PlatesService {
   protected vehicleList: VehicleInfo[]=[
-    {
-      "plate": "90-ON-89",
-      "owner": "João Diogo",
-      "fuel": "Diesel",
-      "photo": "/assets/renault.jpg"
-    },{
-      "plate": "90-10-JK",
-      "owner": "Pedro Diogo",
-      "fuel": "Gasoline",
-      "photo": "/assets/ford.jpg"
-    }
+      {
+        "plate": "90-ON-89",
+        "owner": "João Diogo",
+        "fuel": "Diesel",
+        "photo": "/assets/van2.png"
+      },
+      {
+        "plate": "90-10-JK",
+        "owner": "Pedro Diogo",
+        "fuel": "Gasoline",
+        "photo": "/assets/van2.png"
+      },
+      {
+        "plate": "90-AB-12",
+        "owner": "Maria Silva",
+        "fuel": "Diesel",
+        "photo": "/assets/van2.png"
+      },
+      {
+        "plate": "90-CD-34",
+        "owner": "Ana Costa",
+        "fuel": "Diesel",
+        "photo": "/assets/van2.png"
+      },
+      {
+        "plate": "90-EF-56",
+        "owner": "José Santos",
+        "fuel": "Gasoline",
+        "photo": "/assets/van2.png"
+      },
+      {
+        "plate": "90-GH-78",
+        "owner": "Marta Oliveira",
+        "fuel": "Diesel",
+        "photo": "/assets/van2.png"
+      },
+      {
+        "plate": "90-IJ-90",
+        "owner": "Ricardo Almeida",
+        "fuel": "Diesel",
+        "photo": "/assets/van2.png"
+      },
+      {
+        "plate": "90-KL-12",
+        "owner": "Sofia Rodrigues",
+        "fuel": "Gasoline",
+        "photo": "/assets/van2.png"
+      }
+    
   ];
   constructor() { }
 
