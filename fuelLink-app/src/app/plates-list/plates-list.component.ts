30b0f93b35224901ce3e4c8ef4ddb0d789d99205
CatarinaBarroqueiro <95221731+CatarinaBarroqueiro@users.noreply.github.com>
2024-05-21 11:34:09 +0100

    More mockups and Random Images

--- fuelLink-app/src/app/plates-list/plates-list.component.ts
+++ fuelLink-app/src/app/plates-list/plates-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehicleInfo } from '../vehicle-info';
 import { RouterModule } from '@angular/router';
@@ -9,14 +9,32 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
-      <img class="listing-photo" src='/assets/van2.png' alt="Fuel is {{vehicleInfo.fuel}}">
+      <img class="listing-photo" [src]="randomImageSrc" alt="Fuel is {{vehicleInfo.fuel}}">
       <h2 class="listing-heading">{{vehicleInfo.plate}}</h2>
-      <p class="listing-location"> {{vehicleInfo.fuel}} </p>
+      <p class="listing-location">{{vehicleInfo.fuel}}</p>
       <a [routerLink]="['/details', vehicleInfo.plate]">Learn More</a>
     </section>
   `,
   styleUrls: ['./plates-list.component.css']
 })
-export class PlatesListComponent {
+export class PlatesListComponent implements OnInit {
   @Input() vehicleInfo!: VehicleInfo;
+
+  randomImageSrc!: string;
+
+  private imagePaths: string[] = [
+    '/assets/van1.png',
+    '/assets/van2.png',
+    '/assets/van3.png',
+    '/assets/van4.png'
+  ];
+
+  ngOnInit() {
+    this.randomImageSrc = this.getRandomImage();
+  }
+
+  private getRandomImage(): string {
+    const randomIndex = Math.floor(Math.random() * this.imagePaths.length);
+    return this.imagePaths[randomIndex];
+  }
 }
